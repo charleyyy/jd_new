9dda48541aa68d6a4cf98a813c9f091b28811a42
charleyyy <45123479+charleyyy@users.noreply.github.com>
2021-01-01 23:07:52 +0800

    jr_sign.js 对于已过期活动的脚本,添加通知提醒

--- jr_sign.js
+++ jr_sign.js
@@ -39,6 +39,7 @@ if ($.isNode()) {
   cookiesArr.reverse();
   cookiesArr.push(...[$.getdata('CookieJD2'), $.getdata('CookieJD')]);
   cookiesArr.reverse();
+  cookiesArr = cookiesArr.filter(item => item !== "" && item !== null && item !== undefined);
 }
 const JD_API_HOST = 'https://api.m.jd.com/api';
 !(async () => {
@@ -61,8 +62,6 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
 
         if ($.isNode()) {
           await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
-        } else {
-          $.setdata('', `CookieJD${i ? i + 1 : ""}`);//cookie失效，故清空cookie。$.setdata('', `CookieJD${i ? i + 1 : "" }`);//cookie失效，故清空cookie。
         }
         continue
       }
@@ -79,8 +78,14 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
   })
 
 function showMsg() {
-  return new Promise(resolve => {
-    $.msg($.name, '', `【京东账号${$.index}】${$.nickName}\n${message}`);
+  return new Promise(async resolve => {
+    let nowTime = new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000;
+    if (nowTime > new Date('2020/12/31 23:59:59+08:00').getTime()) {
+      $.msg($.name, '活动已结束', `咱江湖再见\nhttps://github.com/lxk0301/jd_scripts`, {"open-url": "https://github.com/lxk0301/jd_scripts"});
+      if ($.isNode()) await notify.sendNotify($.name + '活动已结束', `咱江湖再见\n https://github.com/lxk0301/jd_scripts`)
+    } else {
+      $.msg($.name, '', `【京东账号${$.index}】${$.nickName}\n${message}`);
+    }
     resolve()
   })
 }
@@ -97,6 +102,7 @@ function sign() {
           if (safeGet(data)) {
             data = JSON.parse(data);
             console.log(data.resultData.message)
+            message += `${data.resultData.message}`
           }
         }
       } catch (e) {
